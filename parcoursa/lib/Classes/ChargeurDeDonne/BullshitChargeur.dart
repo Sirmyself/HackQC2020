29195311f1138941f6cf95909ffdc3707458ef6a
Jerome Masse-Morneau <jerome.masse-morneau@ibm.com>
2020-03-06 22:20:14 -0500

    basic classes....

---
+++ parcoursa/lib/Classes/ChargeurDeDonne/BullshitChargeur.dart
@@ -0,0 +1,15 @@
+import 'package:parcoursa/Interfaces/IChargeDonnees.dart';
+
+class BullshitChargeur implements IChargeDonnees {
+  //à mettre dans la vrai classe si on a le temps
+  /*Map _url_interet_art = {
+    "RIMOUSKI": "",
+    "QUEBEC": ""
+  } */
+
+  @override
+  Set getBalisesArt() {
+    return null;
+  }
+  
+}
---
+++ parcoursa/lib/Interfaces/IChargeDonnees.dart
@@ -0,0 +1,5 @@
+class IChargeDonnees {
+  var _url_interet_art; //just do it like in javascript...
+
+  Set<Marker> getBalisesArt(){}
+}
