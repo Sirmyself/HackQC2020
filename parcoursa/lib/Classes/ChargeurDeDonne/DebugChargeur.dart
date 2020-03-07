0ba6c64db9e68dcc7c19ea46469345529dac0829
Jerome Masse-Morneau <jerome.masse-morneau@ibm.com>
2020-03-07 00:42:43 -0500

    un nom plus professionnel... ça va faire plaisir à Francis

--- parcoursa/lib/Classes/ChargeurDeDonne/DebugChargeur.dart
+++ parcoursa/lib/Classes/ChargeurDeDonne/DebugChargeur.dart
@@ -2,7 +2,7 @@ import 'package:parcoursa/Interfaces/IChargeDonnees.dart';
 
 import '../Balise.dart';
 
-class BullshitChargeur implements IChargeDonnees {
+class DebugChargeur implements IChargeDonnees {
   //à mettre dans la vrai classe si on a le temps
   /*Map _url_interet_art = {
     "RIMOUSKI": "",
