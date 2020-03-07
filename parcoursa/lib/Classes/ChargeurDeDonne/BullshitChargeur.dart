50229e3f91dd9b59352038dbc749b80c0d987eae
Jerome Masse-Morneau <jerome.masse-morneau@ibm.com>
2020-03-06 22:43:52 -0500

    `2e commit pour sync avec Charles

--- parcoursa/lib/Classes/ChargeurDeDonne/BullshitChargeur.dart
+++ parcoursa/lib/Classes/ChargeurDeDonne/BullshitChargeur.dart
@@ -1,15 +1,16 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:parcoursa/Interfaces/IChargeDonnees.dart';
 
 class BullshitChargeur implements IChargeDonnees {
   //à mettre dans la vrai classe si on a le temps
   /*Map _url_interet_art = {
     "RIMOUSKI": "",
-    "QUEBEC": ""
-  } */
+    "QUEBEC": 1""
+  }*/
 
   @override
-  Set getBalisesArt() {
-    return null;
+  Set<Marker> getBalisesArt() {
+    Set<Marker> balises = new Set<Marker>();
+    return Set;
   }
-  
 }
--- parcoursa/lib/Interfaces/IChargeDonnees.dart
+++ parcoursa/lib/Interfaces/IChargeDonnees.dart
@@ -1,3 +1,6 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
 class IChargeDonnees {
   var _url_interet_art; //just do it like in javascript...
 
