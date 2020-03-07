ece20f89b54a32d825d02278fe884575a90072e4
Dave Brochu <dave.b_15@hotmail.com>
2020-03-07 16:53:55 -0500

    Ajout de détection de lieu

---
+++ parcoursa/lib/Classes/Utilisateur.dart
@@ -0,0 +1,11 @@
+Utilisateur utilisateur = Utilisateur();
+
+class Utilisateur {
+  String fullName = "Jérôme Beaulieu";
+  String status = "Niveau: Débutant";
+  String bio =
+    "\"Hi, I am a Freelance developer working for hourly basis. If you wants to contact me to build your product leave a message.\"";
+  String points = "173";
+  String lieuVisites = "24";
+  String sucess = "5";
+}
