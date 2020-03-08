ffb72a05113b591991555d4b763f10e28d574f03
Jerome Masse-Morneau <jerome.masse-morneau@ibm.com>
2020-03-08 11:35:01 -0400

    damnit

--- parcoursa/lib/parcours.dart
+++ parcoursa/lib/parcours.dart
@@ -1,14 +1,12 @@
-
-//import 'package:parcoursa/parcour.dart';
-
 import 'dart:math';
-
 import 'Classes/ChargeurDeDonne/DebugChargeur.dart';
 import 'Interfaces/IChargeDonnees.dart';
+var rng = new Random(23);
 
-var rng = new Random();
+var debug = DebugChargeur();
+var public = debug.getBalisesPublic().first;
+var art = debug.getBalisesArt().first;
 
-IChargeDonnees debug = DebugChargeur();
 var parcRimouski = debug.getBalisesParc(VILLE_SUPPORTE.RIMOUSKI);
 var parcQuebec = debug.getBalisesParc(VILLE_SUPPORTE.QUEBEC);
 var parcMontreal = debug.getBalisesParc(VILLE_SUPPORTE.MONTREAL);
@@ -21,15 +19,75 @@ var publicRimouski = debug.getBalisesPublic(VILLE_SUPPORTE.RIMOUSKI);
 var publicQuebec = debug.getBalisesPublic(VILLE_SUPPORTE.QUEBEC);
 var publicMontreal = debug.getBalisesPublic(VILLE_SUPPORTE.MONTREAL); 
 
+
 List parcours = [
-/*
+  
   {
-    "img": ,
-    "title": ,
-    "address": ,
+    "img": parcRimouski.first.urlImage,
+    "title": parcRimouski.first.nomBalise,
+    "address": parcRimouski.first.description,
     "rating": (rng.nextInt(100) + 25).toString(),
-    "nbrBalise": 
-  },*/
+    "nbrBalise": parcRimouski.length
+  },
+  {
+    "img": artRimouski.first.urlImage,
+    "title": artRimouski.first.nomBalise,
+    "address": artRimouski.first.description,
+    "rating": (rng.nextInt(100) + 25).toString(),
+    "nbrBalise": artRimouski.length
+  },
+  {
+    "img": publicRimouski.first.urlImage,
+    "title": publicRimouski.first.nomBalise,
+    "address": publicRimouski.first.description,
+    "rating": (rng.nextInt(100) + 25).toString(),
+    "nbrBalise": publicRimouski.length
+  },
+
+  
+  {
+    "img": parcQuebec.first.urlImage,
+    "title": parcQuebec.first.nomBalise,
+    "address": parcQuebec.first.description,
+    "rating": (rng.nextInt(100) + 25).toString(),
+    "nbrBalise": parcQuebec.length
+  },
+  {
+    "img": publicQuebec.first.urlImage,
+    "title": publicQuebec.first.nomBalise,
+    "address": publicQuebec.first.description,
+    "rating": (rng.nextInt(100) + 25).toString(),
+    "nbrBalise": publicQuebec.length
+  },
+
+  
+  {
+    "img": parcMontreal.first.urlImage,
+    "title": parcMontreal.first.nomBalise,
+    "address": parcMontreal.first.description,
+    "rating": (rng.nextInt(100) + 25).toString(),
+    "nbrBalise": parcMontreal.length
+  },
+  {
+    "img": artMontreal.first.urlImage,
+    "title": artMontreal.first.nomBalise,
+    "address": artMontreal.first.description,
+    "rating": (rng.nextInt(100) + 25).toString(),
+    "nbrBalise": artMontreal.length
+  },
+  {
+    "img": publicMontreal.first.urlImage,
+    "title": publicMontreal.first.nomBalise,
+    "address": publicMontreal.first.description,
+    "rating": (rng.nextInt(100) + 25).toString(),
+    "nbrBalise": publicMontreal.length
+  }
+];
+/*
+
+
+
+List parcours = [
   
   {
     "img": "assets/profil_1.jpg",
@@ -39,21 +97,21 @@ List parcours = [
     "nbrBalise": 6
   },
   {
-    "img": parcRimouski.first.urlImage.toString(),
+    "img": parcRimouski.first.urlImage,
     "title": parcRimouski.first.nomBalise,
     "address": parcRimouski.first.description,
     "rating": (rng.nextInt(100) + 25).toString(),
     "nbrBalise": parcRimouski.length
   },
   {
-    "img": artRimouski.first.urlImage.toString(),
+    "img": artRimouski.first.urlImage,
     "title": artRimouski.first.nomBalise,
     "address": artRimouski.first.description,
     "rating": (rng.nextInt(100) + 25).toString(),
     "nbrBalise": artRimouski.length
   },
   {
-    "img": publicRimouski.first.urlImage.toString(),
+    "img": publicRimouski.first.urlImage,
     "title": publicRimouski.first.nomBalise,
     "address": publicRimouski.first.description,
     "rating": (rng.nextInt(100) + 25).toString(),
@@ -62,21 +120,21 @@ List parcours = [
 
   
   {
-    "img": parcQuebec.first.urlImage.toString(),
+    "img": parcQuebec.first.urlImage,
     "title": parcQuebec.first.nomBalise,
     "address": parcQuebec.first.description,
     "rating": (rng.nextInt(100) + 25).toString(),
     "nbrBalise": parcQuebec.length
   },/*
   {
-    "img": artQuebec.first.urlImage.toString(),
+    "img": artQuebec.first.urlImage,
     "title": artQuebec.first.nomBalise,
     "address": artQuebec.first.description,
     "rating": (rng.nextInt(100) + 25).toString(),
     "nbrBalise": artQuebec.length
   },*/
   {
-    "img": publicQuebec.first.urlImage.toString(),
+    "img": publicQuebec.first.urlImage,
     "title": publicQuebec.first.nomBalise,
     "address": publicQuebec.first.description,
     "rating": (rng.nextInt(100) + 25).toString(),
@@ -85,24 +143,25 @@ List parcours = [
 
   
   {
-    "img": parcMontreal.first.urlImage.toString(),
+    "img": parcMontreal.first.urlImage,
     "title": parcMontreal.first.nomBalise,
     "address": parcMontreal.first.description,
     "rating": (rng.nextInt(100) + 25).toString(),
     "nbrBalise": parcMontreal.length
   },
   {
-    "img": artMontreal.first.urlImage.toString(),
+    "img": artMontreal.first.urlImage,
     "title": artMontreal.first.nomBalise,
     "address": artMontreal.first.description,
     "rating": (rng.nextInt(100) + 25).toString(),
     "nbrBalise": artMontreal.length
   },
   {
-    "img": publicMontreal.first.urlImage.toString(),
+    "img": publicMontreal.first.urlImage,
     "title": publicMontreal.first.nomBalise,
     "address": publicMontreal.first.description,
     "rating": (rng.nextInt(100) + 25).toString(),
     "nbrBalise": publicMontreal.length
-  },//*/
+  },
 ];
+*/
