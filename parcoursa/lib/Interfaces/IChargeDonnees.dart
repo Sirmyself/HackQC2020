81e2c42f9c51b257eca62c707c3e2f6d5d711cfb
Jerome Masse-Morneau <jerome.masse-morneau@ibm.com>
2020-03-07 22:20:38 -0500

    bfdzlkojhnioukjjm^`zbtnhfd

--- parcoursa/lib/Interfaces/IChargeDonnees.dart
+++ parcoursa/lib/Interfaces/IChargeDonnees.dart
@@ -10,16 +10,7 @@ enum VILLE_SUPPORTE{ // filtre les villes supportées
 abstract class IChargeDonnees {
   //var _url_interet_art; //just do it like in javascript...
 
-<<<<<<< HEAD
   List<Balise> getBalisesPublic([VILLE_SUPPORTE pVille = VILLE_SUPPORTE.RIMOUSKI]);
   List<Balise> getBalisesArt([VILLE_SUPPORTE pVille = VILLE_SUPPORTE.RIMOUSKI]);
   List<Balise> getBalisesParc([VILLE_SUPPORTE pVille = VILLE_SUPPORTE.RIMOUSKI]);
-=======
-  List<Balise> getBalisesPublic();
-  Trajet getTrajetPublic();
-  List<Balise> getBalisesArt();
-  //Trajet getTrajetArt();
-  List<Balise> getBalisesParc();
-  //Trajet getTrajetParc();
->>>>>>> 5b12484095d74dfe178110f4598a523a99839976
 }
